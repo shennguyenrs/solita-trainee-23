e4a8ccbcde60aa3f992dfdecbca2568ee8fb8e8a
Shen Nguyen <shennguyen.redshinobi96@gmail.com>
2023-02-10 15:07:15 +0200

    feat: init frontend with Chakra UI

---
+++ front/pages/_document.tsx
@@ -0,0 +1,25 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      <body>
+        <ColorModeScript />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
